4e1157fbe13fcd9a5baac8722cb7012299d372e5
Nate Schaffner and Venus Bhatia <thx1766@gmail.com>
2022-11-05 01:09:37 -0400

    default swiftUI app without tests

---
+++ wedding/wedding/weddingApp.swift
@@ -0,0 +1,17 @@
+//
+//  weddingApp.swift
+//  wedding
+//
+//  Created by Nate Intel on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct weddingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
