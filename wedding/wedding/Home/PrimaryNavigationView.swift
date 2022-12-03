f12c738a4059462ae5d79843fddf72404c0a4305
Venus Bhatia <190120107016@git.org.in>
2022-12-03 22:21:37 +0530

    Added blur effect (You'll like it)

--- wedding/wedding/Home/PrimaryNavigationView.swift
+++ wedding/wedding/Home/PrimaryNavigationView.swift
@@ -9,61 +9,70 @@ import SwiftUI
 
 struct PrimaryNavigationView: View {
     var body: some View {
-        VStack{
-            Image("savethedate")
-                .resizable()
-                .scaledToFit()
-            ScrollView{
-                VStack{
-                    NavigationView{
-                        VStack{
-                            Text("photobooth")
+        ScrollView{
+            VStack{
+                NavigationView{
+                    VStack{
+                        Image(systemName: "slowmo")
                             .font(.custom("Covered", size: 23))
-                           
-                            PhotoView()}
-                        }
-                        .frame(width: 300, height: 400)
-                        .navigationTitle("Images")
+                        Spacer()
+                        PhotoView()}
+                }
+                .navigationTitle("Images")
+            }
+            .frame(height: 500)
+            NavigationView{
+                HStack{
+                    NavigationLink(destination: Wedding_Ring_View()){
+                        Text("Wedding Ring")
+                            .frame(width: 200, height: 150)
+                        
                     }
-                    .frame(height: 500)
-                    NavigationView{
-                        HStack{
-                            NavigationLink(destination: Wedding_Ring_View()){
-                                Text("Wedding Ring")
-                                    .frame(width: 200, height: 150)
-                                  
-                            }
-                            NavigationLink(destination: Engagement_Ring_View()){
-                                Text("Engagement Ring")
-                                    .frame(width: 200, height: 150)
-                          
-                            }
-                        }
-                        .frame(width: 300, height: 400)
-                    
-                        .navigationTitle("Check out rings")
+                    NavigationLink(destination: Engagement_Ring_View()){
+                        Text("Engagement Ring")
+                            .frame(width: 200, height: 150)
+                        
                     }
-                    .frame(height: 600)
-                    NavigationView{
-                        VStack{
-                            NavigationLink(destination: DetailsView()){
-                                Text("Details")
-                                    .frame(width:200, height: 150)
-                                    
-                            }
-                        }
-                        .navigationTitle("Event Info")
+                }
+                .frame(width: 300, height: 400)
+                
+                .navigationTitle("Check out rings")
+            }
+            .frame(height: 600)
+            NavigationView{
+                VStack{
+                    NavigationLink(destination: DetailsView()){
+                        Text("Details")
+                            .frame(width:200, height: 150)
+                        
                     }
-                    .frame(height: 500)
-                    NavigationView{
-                     NavigationLink(destination: SettingsView()){
-                         VStack{
-                             Text("Settings (Logout)").frame(width:300, height: 150);
-                         }.navigationTitle("Settings")
-                    }
-                    }
-                    .frame(height: 300)
+                }
+                .navigationTitle("Event Info")
+            }
+            .frame(height: 500)
+            NavigationView{
+                NavigationLink(destination: SettingsView()){
+                    VStack{
+                        Text("Settings (Logout)").frame(width:300, height: 150);
+                    }.navigationTitle("Settings")
                 }
             }
+            .frame(height: 300)
         }
+        .overlay(
+            ZStack {
+                Color.clear
+                    .background (.ultraThinMaterial)
+                .blur (radius: 10)
+                Text ("Save The Date")
+                .font(.largeTitle.weight(.bold))
+                .frame (maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 20)
+                
+            }
+               
+                .frame (height: 90)
+                .frame(maxHeight: .infinity, alignment: .top)
+        )
     }
+        }
